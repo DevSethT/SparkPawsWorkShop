568dc64ed56f3771c945a6c158b599214ddc547c
Seth Taylor <Taylorseth59@gmail.com>
2026-06-26 11:27:51 -0500

    projects page change to fix rendering

--- src/componates/ProjectCard/ProjectCard.jsx
+++ src/componates/ProjectCard/ProjectCard.jsx
@@ -6,7 +6,7 @@ function ProjectCard({ project }) {
     <Link className="projectCard" to={`/projects/${project.id}`}>
       <h2 className="projectCard__title">{project.name}</h2>
       <div className="projectCard__image-container">
-        <img src={project.images[0]} alt={`${project.name} image`} className="projectCard__image" />
+        <img src={project.images[0] || ""} alt={`${project.name} image`} className="projectCard__image" />
       </div>
       <div className="projectCard__details">
       <p className="projectCard__type">Type: {project.TypeOfProject}</p>
