520dbc15e0637a7da0eb1c61bb5b929c67be776d
Seth Taylor <Taylorseth59@gmail.com>
2026-08-14 20:38:43 -0500

    small updates at work

--- src/Pages/TRPage/TRPage.jsx
+++ src/Pages/TRPage/TRPage.jsx
@@ -4,9 +4,13 @@ import Header from "../../Components/Header/Header";
 
 function TRPage(){
     return (
-        <div>
-            <Header />
-            <h1>TR Page</h1>
+        <div className="trPage">
+            <Header className="trPageHeader"/>
+            <h1>Welcome to the Teardown & Repair hub</h1>
+            <div className="trprojects">
+                {/* Project cards will be displayed here */}
+            </div>
+            
         </div>
     );
 }
